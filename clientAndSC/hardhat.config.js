f607189814674cd86c0621cf9cead9a296f6fe62
Jan Robel <74827759+janekrobel@users.noreply.github.com>
2023-04-21 11:25:48 +0200

    Update hardhat.config.js

--- clientAndSC/hardhat.config.js
+++ clientAndSC/hardhat.config.js
@@ -10,7 +10,7 @@ module.exports = {
   networks: {
     goerli: {
       url: "https://goerli.infura.io/v3/a89fc1de7ad2450da2ca305a134730be",
-      accounts: ['0x940cecb3c0fee1850415258f51bceb66b773d2b1470fb4c69bfa7f8a864dd2d8'],
+      accounts: ['privateKey'],
 
     }
   }
